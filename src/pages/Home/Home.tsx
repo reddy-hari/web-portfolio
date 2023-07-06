b93d8e555b8f4bb9b7d222d3a5020ccb618c5ed9
Hari <harireddy05@gmail.com>
2023-07-06 14:47:47 -0400

    [Resume] Genesis

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,9 @@
-import React from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Navigation from "../../common/Navigation";
 import {
   decrement,
   increment,
 } from "../../reducers/counterReducer/counterSlice";
-import ThemeWrapper from "../../common/ThemeWrapper";
 
 function Home() {
   const counter = useSelector((state: any) => state.counter.value);
